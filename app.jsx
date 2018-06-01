0b5649e5b667fb283584278e457947bce7622f49
John Blaine <jablaine@uw.edu>
2018-06-01 15:57:51 -0600

    Add another example player to scoreboard

--- app.jsx
+++ app.jsx
@@ -6,6 +6,7 @@ function Application() {
       </div>
 
       <div className="players">
+
         <div className="player">
           <div className="player-name">
             John Blaine
@@ -18,6 +19,20 @@ function Application() {
             </div>
           </div>
         </div>
+
+        <div className="player">
+          <div className="player-name">
+            John Blaine
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
