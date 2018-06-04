200b40b3aa85f09f4ca347bce51cba85a0057594
John Blaine <jablaine@uw.edu>
2018-06-04 15:12:35 -0600

    Factor Player section of Application out into its own component

--- src/app.jsx
+++ src/app.jsx
@@ -14,25 +14,29 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-score">
+        <div className="counter">
+          <button className="counter-action decrement"> - </button>
+          <div className="counter-score"> {props.score} </div>
+          <button className="counter-action increment"> + </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function Application(props) {
   return (
     <div className="scoreboard">
       <Header title={props.title}/>
 
       <div className="players">
-
-        <div className="player">
-          <div className="player-name">
-            John Blaine
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 31 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
 
         <div className="player">
           <div className="player-name">
