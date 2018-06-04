b9cba61db353dc23f7a9842c88a1d7bfab195f32
John Blaine <jablaine@uw.edu>
2018-06-04 15:33:08 -0600

    Replace code for players in Application with Player components

--- src/app.jsx
+++ src/app.jsx
@@ -42,20 +42,8 @@ function Application(props) {
       <Header title={props.title}/>
 
       <div className="players">
-
-        <div className="player">
-          <div className="player-name">
-            John Blaine
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 31 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
-        
+        <Player name="John Blaine" score={31}/>
+        <Player name="Nga Nguyen" score={29}/>
       </div>
     </div>
   );
