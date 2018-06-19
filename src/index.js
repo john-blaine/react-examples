07ca13707bcdf9450048c216be2e6d2f552c49a6
John Blaine <jablaine@uw.edu>
2018-06-19 14:26:00 -0600

    Add PLAYERS to index.js and pass in, specify import
    Specifying the import as './Scoreboard.jsx' rather than
    './Scoreboard' fixed a bug that prevented the component
    from being imported

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Scoreboard from './Scoreboard';
+import Scoreboard from './Scoreboard.jsx';
+
+
+const PLAYERS = [
+  {
+    name: "John Blaine",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Nga Nguyen",
+    score: 29,
+    id: 2,
+  },
+  {
+    name: "My Hao Pham",
+    score: 37,
+    id: 3,
+  },
+]
 
 render(
-  <Scoreboard />,
+  <Scoreboard initialPlayers={PLAYERS}/>,
   document.getElementById('root')
 );
-
-
-
