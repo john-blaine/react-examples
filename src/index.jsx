bfa87d1b000a56684de5e40aba278ba35c2c9707
John Blaine <jablaine@uw.edu>
2018-06-19 14:51:37 -0600

    Change index file extension, lint file

--- src/index.jsx
+++ src/index.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import { render } from 'react-dom';
 import Scoreboard from './Scoreboard.jsx';
 
-
 const PLAYERS = [
   {
-    name: "John Blaine",
+    name: 'John Blaine',
     score: 31,
     id: 1,
   },
   {
-    name: "Nga Nguyen",
+    name: 'Nga Nguyen',
     score: 29,
     id: 2,
   },
   {
-    name: "My Hao Pham",
+    name: 'My Hao Pham',
     score: 37,
     id: 3,
   },
-]
+];
 
 render(
-  <Scoreboard initialPlayers={PLAYERS}/>,
-  document.getElementById('root')
+  <Scoreboard initialPlayers={PLAYERS} />,
+  document.getElementById('root'),
 );
