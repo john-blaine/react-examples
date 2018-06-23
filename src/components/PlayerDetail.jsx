b51ccfeb2d47ccfc4b9b18f7c122e05485f6cc2a
John Blaine <jablaine@uw.edu>
2018-06-23 08:19:47 -0600

    Complete PlayerDetail component

--- src/components/PlayerDetail.jsx
+++ src/components/PlayerDetail.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PlayerDetail = ({ }) => {
-  if (){
+const PlayerDetail = ({index, player}) => {
+  if (index > -1){
     return (
       <div>
-        <h3>{ }</h3>
+        <h3>{ player.name }</h3>
         <ul>
           <li>
             <span>Score: </span>
-            { }
+            { player.score }
           </li>
           <li>
             <span>Created: </span>
-            { }
+            { player.created }
           </li>
           <li>
             <span>Updated: </span>
+            { player.updated }
           </li>
         </ul>
       </div>
